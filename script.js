f08077486178aa480d0728beb90bf45553295098
vksingh1421 <119750946+vksingh1421@users.noreply.github.com>
2023-07-03 09:12:18 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+function generateCat() {
+    var image = document.createElement('img');
+    var div = document.getElementById('flex-cat-gen');
+    image.src = "https://giphy.com/explore/cat";
+    div.appendChild(image);
+}
